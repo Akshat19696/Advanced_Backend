2d0af6a38592825cb5b522908a3eabb4d796692e
Akshat19696 <akshatpal19696@gmail.com>
2026-08-21 10:49:49 +0530

    format code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 
-(async()=>{
-try{
-await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-}catch(error){
-    console.error("Error:",error);
-}
-})()
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+})();
