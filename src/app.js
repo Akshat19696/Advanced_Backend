22cef475b3caf1076b719f41eb59a958c0952d5e
Akshat19696 <akshatpal19696@gmail.com>
2026-08-25 09:21:18 +0530

    some more important configurations required to setup backend

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIgiGIN,
+    credentials: true,
+  }),
+);
+
+// 3 major configuration we generally do to setup-backend
+app.use(express.json({ limit: "16kb" })); //default 100 kb hoga agar nahi set hoga to
+app.use(express.urlencoded({ extended: true })); //it now use qs library to parse the url-encoded data if false
+//by default using query string library
+app.use(express.static("public"));
+app.use(cookieParser());//taki server cient ko cookie bhej sake usme crud laga sake
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import connectDB from "./db/index.js";
+import app from "./app.js";
+import "dotenv/config"
 
+connectDB().then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`app is listening on port ${process.env.PORT}`);
+    })
 
-connectDB();
+}).catch((error)=>{
+    console.log("MongoDb failed to connect",error);
+})
