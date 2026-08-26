ae056585ab7dedc8e7ed00846f5df10775d3a753
Akshat19696 <akshatpal19696@gmail.com>
2026-08-26 08:54:24 +0530

    implemented bcrypt npm package for hashing password....

---
+++ src/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import jwt from "JsonWebToken";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url
+    },
+    watchHistory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+  } else {
+    return next();
+  }
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
